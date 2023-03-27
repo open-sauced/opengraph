8c1adf074a7271d02846b3413eb8e909d0282f86
NightKnight <Ahmedatwa866@yahoo.com>
2023-03-28 00:41:36 +0200

    feat: use node https instead of axios

--- src/fetchers/ProfileCardFetcher.ts
+++ src/fetchers/ProfileCardFetcher.ts
@@ -1,12 +1,32 @@
 import axios from "axios";
+import https from "https";
 import { TopicContributionEndpoint } from "../utils/types/dtos.types";
 
 export default async function ProfileCardDataFetcher (name: string): Promise<{ langs: string[], repos: string[], img: string }> {
-  const req = await axios<TopicContributionEndpoint>(`https://beta.gs-api.opensauced.pizza/v1/*/contributions?page=1&limit=1&range=30&contributor=${name}`);
+  const reqData = await new Promise<TopicContributionEndpoint>((resolve, reject) => {
+    https
+      .get(`https://beta.gs-api.opensauced.pizza/v1/*/contributions?page=1&limit=1&range=30&contributor=${name}`, resp => {
+        let data = "";
 
-  console.log(req.data);
-  const contributor = req.data.data[0];
-  
+        // a chunk of data has been recieved.
+        resp.on("data", chunk => {
+          data += chunk;
+        });
+
+        // the whole response has been received. Print out the result.
+        resp.on("end", () => {
+          resolve(JSON.parse(data));
+        });
+      })
+      .on("error", err => {
+        console.log(`Error: ${err.message}`);
+        reject(err);
+      });
+  });
+
+  console.log(reqData);
+  const contributor = reqData.data[0];
+
   // could be undefined if array is empty
   if (!contributor) {
     throw new Error(`User '${name}' Not Found`);
