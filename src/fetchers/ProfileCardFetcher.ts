b50051309006efe060c3a844bc74e391729376b6
NightKnight <Ahmedatwa866@yahoo.com>
2023-03-28 16:02:26 +0200

    feat: repalce https module with fetch

--- src/fetchers/ProfileCardFetcher.ts
+++ src/fetchers/ProfileCardFetcher.ts
@@ -1,27 +1,8 @@
-import { https } from "follow-redirects";
 import { TopicContributionEndpoint } from "../utils/types/dtos.types";
 
 export default async function ProfileCardDataFetcher (name: string): Promise<{ langs: string[], repos: string[], img: string }> {
-  const reqData = await new Promise<TopicContributionEndpoint>((resolve, reject) => {
-    https
-      .get(`https://beta.gs-api.opensauced.pizza/v1/*/contributions?page=1&limit=1&range=30&contributor=${name}`, resp => {
-        let data = "";
-
-        // a chunk of data has been recieved.
-        resp.on("data", chunk => {
-          data += chunk;
-        });
-
-        // the whole response has been received. Print out the result.
-        resp.on("end", () => {
-          resolve(JSON.parse(data) as TopicContributionEndpoint);
-        });
-      })
-      .on("error", err => {
-        console.log(`Error: ${err.message}`);
-        reject(err);
-      });
-  });
+  const req = await fetch(`https://beta.gs-api.opensauced.pizza/v1/*/contributions?page=1&limit=1&range=30&contributor=${name}`);
+  const reqData = await req.json() as TopicContributionEndpoint;
 
   console.log(reqData);
   const contributor = reqData.data[0];
@@ -34,28 +15,10 @@ export default async function ProfileCardDataFetcher (name: string): Promise<{ l
   const langs = contributor.langs ? contributor.langs.split(",") : [];
   const repos = contributor.recent_repo_list ? contributor.recent_repo_list.split(",") : [];
 
-  const imgReqData = await new Promise<string>((resolve, reject) => {
-    https
-      .get(`https://www.github.com/${name}.png?size=300`, res => {
-        const data: Uint8Array[] = [];
+  const imgReq = await fetch(`https://www.github.com/${name}.png?size=300`);
+  const imgReqBody = await imgReq.blob();
 
-        res.on("data", d => {
-          data.push(d as Uint8Array);
-        });
-        res.on("end", () => {
-          const buffer = Buffer.concat(data);
-          const base64String = buffer.toString("base64");
-
-          resolve(base64String);
-        });
-      })
-      .on("error", err => {
-        console.log(`Error: ${err.message}`);
-        reject(err);
-      });
-  });
-
-  const img = `data:image/jpeg;base64,${imgReqData}`;
+  const img = URL.createObjectURL(imgReqBody);
 
   return {
     langs,
