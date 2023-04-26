d8f71683b0edd8ab45c1645321c54104e32d8493
Deadreyo <Deadreyo@users.noreply.github.com>
2023-04-26 11:39:13 +0000

    feat: adding utilities for cards local generation & testing (#34)
    * feat: getuserdata private
    
    * fix: fix incorrect import
    
    * feat: refractor & add local scripts
    
    * feat: add local dev generation script
    
    * last touches
    
    * refractor: suggestions
    
    * docs: add documentation for local dev script
    
    * refractor: change output folder
    
    * update local dev user command name c5a5fecd3689b11fd3325c7f0fd3228bd368e0f5

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -158,6 +158,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             <li class="link">
                                 <a href="interfaces/RequiresUpdateMeta.html" data-type="entity-link" >RequiresUpdateMeta</a>
                             </li>
+                            <li class="link">
+                                <a href="interfaces/UserCardData.html" data-type="entity-link" >UserCardData</a>
+                            </li>
                         </ul>
                     </li>
                     <li class="chapter">
